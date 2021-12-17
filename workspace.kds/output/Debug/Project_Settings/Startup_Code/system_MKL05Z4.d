cf5158fc38a39bda8c6cdd0aee9ccc33a1fef059
yellowfox-star-is <yellowfox.star.is@gmail.com>
2021-12-18 00:03:34 +0100

    project storage

---
+++ workspace.kds/output/Debug/Project_Settings/Startup_Code/system_MKL05Z4.d
@@ -0,0 +1,14 @@
+Project_Settings/Startup_Code/system_MKL05Z4.o: \
+ ../Project_Settings/Startup_Code/system_MKL05Z4.c ../Includes/MKL05Z4.h \
+ ../Includes/core_cm0plus.h ../Includes/core_cmInstr.h \
+ ../Includes/core_cmFunc.h ../Includes/system_MKL05Z4.h
+
+../Includes/MKL05Z4.h:
+
+../Includes/core_cm0plus.h:
+
+../Includes/core_cmInstr.h:
+
+../Includes/core_cmFunc.h:
+
+../Includes/system_MKL05Z4.h:
---
+++ workspace.kds/output/Debug/Sources/main.d
@@ -0,0 +1,13 @@
+Sources/main.o: ../Sources/main.c ../Includes/MKL05Z4.h \
+ ../Includes/core_cm0plus.h ../Includes/core_cmInstr.h \
+ ../Includes/core_cmFunc.h ../Includes/system_MKL05Z4.h
+
+../Includes/MKL05Z4.h:
+
+../Includes/core_cm0plus.h:
+
+../Includes/core_cmInstr.h:
+
+../Includes/core_cmFunc.h:
+
+../Includes/system_MKL05Z4.h:
